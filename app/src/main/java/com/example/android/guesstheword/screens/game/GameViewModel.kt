14b1331fd9fe66aff6c273328cad5be9055af290
kevinmainairungu <mainairungu99@gmail.com>
2020-06-02 19:15:31 +0300

    added a viewmodel class

---
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -0,0 +1,11 @@
+package com.example.android.guesstheword.screens.game
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import timber.log.Timber
+
+class GameViewModel : ViewModel(){
+    init {
+        Timber.i("GameViewModel created")
+    }
+}
