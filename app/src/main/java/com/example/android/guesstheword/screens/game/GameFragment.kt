90454c454332eb0bbfc39bccc0972fa414b79825
kevinmainairungu <mainairungu99@gmail.com>
2020-06-03 15:34:32 +0300

    Moved the logic in the game fragment to the gameviewmodel. the view model contains all the data that needs to be saved and updated including the methods that do so.

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
@@ -21,9 +21,12 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.NavHostFragment.findNavController
 import com.example.android.guesstheword.R
 import com.example.android.guesstheword.databinding.GameFragmentBinding
@@ -43,14 +46,7 @@ class GameFragment : Fragment() {
 //    make a field for the new ViewModel
 
     private lateinit var viewModel: GameViewModel
-    // The current word
-    private var word = ""
 
-    // The current score
-    private var score = 0
-
-    // The list of words - the front of the list is the next word to guess
-    private lateinit var wordList: MutableList<String>
 
     private lateinit var binding: GameFragmentBinding
 
@@ -66,105 +62,62 @@ class GameFragment : Fragment() {
         )
 // viewModel = ViewModelProviders.of(this).get(GameViewModel::class.java) earlier versions of the ViewModel class and it has been deprecated
 
-        Timber.i("Called ViewModel")
+        Timber.i("Called ViewModelProviders of")
         viewModel = ViewModelProvider(this).get(GameViewModel::class.java)
 
-        resetList()
-        nextWord()
 
-        binding.correctButton.setOnClickListener { onCorrect() }
-        binding.skipButton.setOnClickListener { onSkip() }
+        binding.correctButton.setOnClickListener {
+            viewModel.onCorrect()
+            updateScoreText()
+            updateWordText()
+        }
+        binding.skipButton.setOnClickListener {
+            onSkip()
+            updateScoreText()
+            updateWordText()
+        }
         updateScoreText()
         updateWordText()
-
-        if (savedInstanceState != null) {
-            word = savedInstanceState.getString(WORD_VALUE, "")
-            score = savedInstanceState.getInt(SCORE_VALUE, score)
-            updateWordText()
-            updateScoreText()
-        }
         return binding.root
 
     }
 
-    /**
-     * Resets the list of words and randomizes the order
-     */
-    private fun resetList() {
-        wordList = mutableListOf(
-                "queen",
-                "hospital",
-                "basketball",
-                "cat",
-                "change",
-                "snail",
-                "soup",
-                "calendar",
-                "sad",
-                "desk",
-                "guitar",
-                "home",
-                "railway",
-                "zebra",
-                "jelly",
-                "car",
-                "crow",
-                "trade",
-                "bag",
-                "roll",
-                "bubble"
-        )
-        wordList.shuffle()
-    }
-
-    /**
-     * Called when the game is finished
-     */
-    private fun gameFinished() {
-        val action = GameFragmentDirections.actionGameToScore(score)
-        findNavController(this).navigate(action)
-    }
-
-    /**
-     * Moves to the next word in the list
-     */
-    private fun nextWord() {
-        //Select and remove a word from the list
-        if (wordList.isEmpty()) {
-            gameFinished()
-        } else {
-            word = wordList.removeAt(0)
-        }
-        updateWordText()
-        updateScoreText()
-    }
 
     /** Methods for buttons presses **/
 
     private fun onSkip() {
-        score--
-        nextWord()
+        viewModel.onSkip()
+        updateWordText()
+        updateScoreText()
     }
 
     private fun onCorrect() {
-        score++
-        nextWord()
+        viewModel.onCorrect()
+        updateWordText()
+        updateScoreText()
+    }
+
+    private fun GameFinished() {
+        Toast.makeText(activity, "Game has just finished", Toast.LENGTH_LONG).show()
+        val action = GameFragmentDirections.actionGameToScore(viewModel.score)
+        NavHostFragment.findNavController(this).navigate(action)
     }
 
     /** Methods for updating the UI **/
 
     private fun updateWordText() {
-        binding.wordText.text = word
+        binding.wordText.text = viewModel.word
 
     }
 
     private fun updateScoreText() {
-        binding.scoreText.text = score.toString()
+        binding.scoreText.text = viewModel.score.toString()
     }
-    override fun onSaveInstanceState(outState: Bundle) {
-        outState.putString(WORD_VALUE, word)
-        outState.putInt(SCORE_VALUE, score)
-        super.onSaveInstanceState(outState)
+//    function to endGame
+    private fun onEndGame(){
+        GameFinished()
     }
+
+
 
 }
--- app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -4,13 +4,75 @@ import androidx.lifecycle.ViewModel
 import timber.log.Timber
 
 class GameViewModel : ViewModel() {
-    init {
-        Timber.i("GameViewModel has been called")
+
+     var word = ""
+
+    // The current score
+     var score = 0
+
+    // The list of words - the front of the list is the next word to guess
+    private lateinit var wordList: MutableList<String>
+
+
+    private fun resetList() {
+        wordList = mutableListOf(
+                "queen",
+                "hospital",
+                "basketball",
+                "cat",
+                "change",
+                "snail",
+                "soup",
+                "calendar",
+                "sad",
+                "desk",
+                "guitar",
+                "home",
+                "railway",
+                "zebra",
+                "jelly",
+                "car",
+                "crow",
+                "trade",
+                "bag",
+                "roll",
+                "bubble"
+        )
+        wordList.shuffle()
     }
+
+    init {
+        resetList()
+        nextWord()
+    }
+
+
+    /**
+     * Moves to the next word in the list
+     */
+    private fun nextWord() {
+        //Select and remove a word from the list
+        if (wordList.isEmpty()) {
+//            GameFinished()
+        } else {
+            word = wordList.removeAt(0)
+        }
+    }
+
+    fun onSkip() {
+        score--
+        nextWord()
+    }
+
+    fun onCorrect() {
+        score++
+        nextWord()
+    }
+
 //this is called every time the ViewModel is about to be destroyed
 
     override fun onCleared() {
         super.onCleared()
-        Timber.i("GameViewModel has been destroyed")
+        Timber.i("GameViewModel destroyed")
     }
 }
