4e52ac82630d91301dbd44f2af6de7ec83f9f7f3
kevinmainairungu <mainairungu99@gmail.com>
2020-08-28 23:37:41 +0300

    ViewModel Factory

--- app/src/main/java/com/example/android/guesstheword/screens/score/ScoreViewModelFactory.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/score/ScoreViewModelFactory.kt
@@ -2,9 +2,12 @@ package com.example.android.guesstheword.screens.score
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import timber.log.Timber
+
 // creating the viewModel factory with view Members
 class ScoreViewModelFactory(private val finalScore: Int) : ViewModelProvider.Factory {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        Timber.i("viewModelFactory invoked")
         TODO("Not yet implemented")
     }
 }
